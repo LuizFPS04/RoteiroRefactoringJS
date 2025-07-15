79e4136c65c1f7989f18b3ac4ccb4c56536469c1
LUIZ FELIPE PEREIRA DE SENA <0079513@IFMG.EDU.BR>
2025-07-15 09:48:51 -0300

    Commit 3 - Mais Extract Functions

--- index.js
+++ index.js
@@ -4,16 +4,10 @@ function gerarFaturaStr(fatura, pecas) {
   let totalFatura = 0;
   let creditos = 0;
   let faturaStr = `Fatura ${fatura.cliente}\n`;
-  const formato = new Intl.NumberFormat("pt-BR",
-    {
-      style: "currency", currency: "BRL",
-      minimumFractionDigits: 2
-    }).format;
 
   function calcularTotalApresentacao(apre) {
-    const peca = getPeca(apre);
     let total = 0;
-    switch (peca.tipo) {
+    switch (getPeca(apre).tipo) {
       case "tragedia":
         total = 40000;
         if (apre.audiencia > 30) {
@@ -37,6 +31,20 @@ function gerarFaturaStr(fatura, pecas) {
     return pecas[apresentacao.id];
   }
 
+  function calcularCredito(apre) {
+    let creditos = 0;
+    creditos += Math.max(apre.audiencia - 30, 0);
+    if (getPeca(apre).tipo === "comedia") 
+       creditos += Math.floor(apre.audiencia / 5);
+    return creditos;   
+  }
+
+  function formatarMoeda(valor) {
+    return new Intl.NumberFormat("pt-BR",
+      { style: "currency", currency: "BRL",
+        minimumFractionDigits: 2 }).format(valor/100);
+  }
+
   for (let apre of fatura.apresentacoes) {
 
     const peca = getPeca(apre);
@@ -44,15 +52,13 @@ function gerarFaturaStr(fatura, pecas) {
     let total = calcularTotalApresentacao(apre);
 
     // créditos para próximas contratações
-    creditos += Math.max(apre.audiencia - 30, 0);
-    if (peca.tipo === "comedia")
-      creditos += Math.floor(apre.audiencia / 5);
+    creditos = calcularCredito(apre);
 
     // mais uma linha da fatura
-    faturaStr += `  ${peca.nome}: ${formato(total / 100)} (${apre.audiencia} assentos)\n`;
+    faturaStr += `  ${peca.nome}: ${formatarMoeda(total)} (${apre.audiencia} assentos)\n`;
     totalFatura += total;
   }
-  faturaStr += `Valor total: ${formato(totalFatura / 100)}\n`;
+  faturaStr += `Valor total: ${formatarMoeda(totalFatura)}\n`;
   faturaStr += `Créditos acumulados: ${creditos} \n`;
   return faturaStr;
 }
